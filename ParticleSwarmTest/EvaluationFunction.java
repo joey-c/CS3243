561971a448af9c421936112d4dd23a86a1f684ba
Jerome Cheng <jerome@ayulin.net>
2014-03-26 14:15:10 +0800

    Formatting.

--- ParticleSwarmTest/EvaluationFunction.java
+++ ParticleSwarmTest/EvaluationFunction.java
@@ -3,17 +3,11 @@ package ParticleSwarmTest;
 import net.sourceforge.jswarm_pso.FitnessFunction;
 
 enum Weight {
-	LANDING_HEIGHT(0),
-	ROWS_CLEARED(1),
-	ROW_TRANSITIONS(2),
-	COLUMN_TRANSITIONS(3),
-	HOLES(4),
-	CUMULATIVE_WELLS(5),
-	HOLE_DEPTH(6),
-	ROW_HOLES(7);
-	
+	LANDING_HEIGHT(0), ROWS_CLEARED(1), ROW_TRANSITIONS(2), COLUMN_TRANSITIONS(
+			3), HOLES(4), CUMULATIVE_WELLS(5), HOLE_DEPTH(6), ROW_HOLES(7);
+
 	public final int Value;
-	
+
 	private Weight(int value) {
 		Value = value;
 	}
@@ -51,31 +45,29 @@ public class EvaluationFunction extends FitnessFunction {
 			int[] currentMove = moves[i];
 			int orientation = currentMove[0];
 			int column = currentMove[1];
-			
+
 			StateTester clonedState = StateTester.fromGame(currentGame);
 			clonedState.makeMove(orientation, column);
-			
-			// We want to evaluate this move based on the 8 characteristics:
+
 			// 1. Landing Height
 			// 3. Row Transitions
 			// 4. Column Transitions
 			// 5. Holes
 			// 6. Cumulative Wells
-			// 7. Hole Depth
 			// 8. Row Holes
-			
-			double weightedScore = 0; // This should contain the sum of all the characteristics.
-			
+
+			double weightedScore = 0; 
+
 			// 2. Rows Cleared
 			double rowsClearedScore = calculateRowsClearedScore(weights,
 					clonedState);
 			weightedScore += rowsClearedScore;
-			
+
 			// 7. Hole Depth
 			double holeDepthScore = calculateHoleDepthScore(weights,
 					clonedState);
 			weightedScore += holeDepthScore;
-			
+
 			// If this move scores better than the previous ones,
 			// set it as our choice.
 			if (weightedScore > currentHighScore) {
@@ -94,7 +86,7 @@ public class EvaluationFunction extends FitnessFunction {
 		int[][] field = clonedState.getField();
 		int totalHoleDepth = countHoleDepth(field);
 
-		double score = (double)totalHoleDepth * holeDepthWeight;
+		double score = (double) totalHoleDepth * holeDepthWeight;
 		return score;
 	}
 
@@ -122,7 +114,8 @@ public class EvaluationFunction extends FitnessFunction {
 	private double calculateRowsClearedScore(double[] weights,
 			StateTester clonedState) {
 		double rowsClearedWeight = weights[Weight.ROWS_CLEARED.Value];
-		double rowsClearedScore = clonedState.rowsClearedAfterMove * rowsClearedWeight;
+		double rowsClearedScore = clonedState.rowsClearedAfterMove
+				* rowsClearedWeight;
 		return rowsClearedScore;
 	}
 
