428a66b40459eccb024b63fb501e0661b63ed86d
Jerome Cheng <jerome@ayulin.net>
2014-04-03 17:10:48 +0800

    Cap weights at +- 10 for now.

--- ParticleSwarmTest/SwarmTester.java
+++ ParticleSwarmTest/SwarmTester.java
@@ -6,11 +6,9 @@ public class SwarmTester {
 
 	public static void main(String[] args) {
 		Swarm swarm = new Swarm(Swarm.DEFAULT_NUMBER_OF_PARTICLES, new WeightParticle(), new EvaluationFunction());
-		swarm.setMaxPosition(20); // The maximum any one weight could be.
-		swarm.setMinPosition(-20); // The minimum any one weight could be.
+		swarm.setMaxPosition(10); // The maximum any one weight could be.
+		swarm.setMinPosition(-10); // The minimum any one weight could be.
 		
-		// We may need to set velocity increments as well? The defaults
-		// are 0.9, which seem rather high.
 		
 		int maxSwarmIterations = 10000;
 		for (int i = 0; i < maxSwarmIterations; i++) {
